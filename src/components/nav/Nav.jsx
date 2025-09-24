69dca5786e673de7aaf888790c610220fa81956a
iamhharish <iamhharish@gmail.com>
2025-09-24 21:49:20 +0530

    Made Route paths

---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div>
+     
+      <a href="/">Dashboard</a>
+      <a href="/login">Login</a>
+    </div>
+  );
+};
+
+export default Nav;
