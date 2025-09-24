91bd7d4c859d8b959d5526b967fb4b32c1a9a3f1
WhisperedShadow <dd9514371@gmail.com>
2025-09-24 21:54:50 +0530

    This is A fun msg

--- src/pages/dashboard/Dash.jsx
+++ src/pages/dashboard/Dash.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import styles from "./Dash.module.css";
 
 const Dash = () => {
   return (
-    <div>Dash</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.heading}>
+        Harish is <span className={styles.highlight}>GAYYYY!!!</span>
+      </h1>
+    </div>
+  );
+};
 
-export default Dash
+export default Dash;
