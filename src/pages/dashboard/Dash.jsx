686ca04a35f839abf8439f5f95afeb7d83d4b26f
WhisperedShadow <dd9514371@gmail.com>
2025-09-24 21:59:49 +0530

    This is A fun msg

--- src/pages/dashboard/Dash.jsx
+++ src/pages/dashboard/Dash.jsx
@@ -3,9 +3,7 @@ import styles from "./Dash.module.css";
 const Dash = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.heading}>
-        Harish is <span className={styles.highlight}>GAYYYY!!!</span>
-      </h1>
+      <h1 className={styles.heading}>{styles.container}</h1>
     </div>
   );
 };
